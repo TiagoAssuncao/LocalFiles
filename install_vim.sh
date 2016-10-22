481d95979a0aa168105dc4fed09fd06389fb1d7c
Tiago Assuncao <thiagoribeironiquel@hotmail.com>
2016-10-22 17:32:58 +0000

    Adding new vimrc

---
+++ install_vim.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/TiagoAssuncao/LocalFiles
+cp LocalFiles/new_vimrc .vimrc
+vim +PluginInstall +qall
+git clone https://github.com/powerline/fonts
+cd fonts/
+sh install.sh
