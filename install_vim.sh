1d9f1da9b802ddbca23c7bbdd7a683a58e807b49
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2016-10-23 17:21:14 -0200

    Adding colorscheme to new vim

--- install_vim.sh
+++ install_vim.sh
@@ -5,3 +5,9 @@ vim +PluginInstall +qall
 git clone https://github.com/powerline/fonts
 cd fonts/
 sh install.sh
+
+touch ~/.tmux.conf
+echo "set -g default-terminal 'screen-256color'" >> ~/.tmux.conf
+echo "alias tmux='TERM=screen-256color-bce tmux' " >> ~/.bashrc
+source ~/.bashrc
+
